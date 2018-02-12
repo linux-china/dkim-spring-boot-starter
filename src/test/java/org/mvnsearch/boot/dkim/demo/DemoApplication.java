cedd4f3ac5e237ee2ab9fa28f46eb50b88a00a59
linux_china <linux_china@hotmail.com>
2018-02-12 17:51:46 +0800

    demo spring boot application

---
+++ src/test/java/org/mvnsearch/boot/dkim/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package org.mvnsearch.boot.dkim.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * demo spring boot application
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
